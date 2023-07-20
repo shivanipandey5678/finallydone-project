aa151ded237068b41ee9014bb176443836a4ce50
shivanipandey5678 <140058874+shivanipandey5678@users.noreply.github.com>
2023-07-21 01:43:15 +0530

    Add files via upload

---
+++ new one project.py
@@ -0,0 +1,30 @@
+# from sketchpy import canvas
+# obj = canvas.sketch_from_image("E:\large-lshiv.webp")
+# obj.draw(threshold=75)
+#  # 2nd one
+# from sketchpy import canvas
+# obj = canvas.sketch_from_image('E:\shivaaa.jpg')
+# obj.draw(threshold=95)
+# 3rd
+# from sketchpy import canvas
+#
+#
+# obj = canvas.sketch_from_image('E:\shiva.jpg')
+# obj.draw(threshold=80)
+# 4th one turtle drawing
+import turtle
+
+import turtle
+t=turtle.Turtle()
+list1=["pink","orange","yellow","red"]
+for i in range(20):
+    t.left(20)
+    for j in range(4):
+        t.color(list1[j])
+        t.pensize(2)
+        t.forward(100)
+
+        t.left(90)
+
+
+
